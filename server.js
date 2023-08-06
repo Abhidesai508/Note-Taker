71809a982d387cd8fb3840e0d6b4bd14e5c3a038
Abhishek Desai <109991507+Abhidesai508@users.noreply.github.com>
2023-08-06 01:04:22 -0400

    Made the changes in server

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
-const api = require('./routes/routes.js');
+const fs = require('fs');
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
@@ -10,9 +10,63 @@ const app = express();
 //Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/api', api);
+
+// app.use('/api', api);
 
 app.use(express.static('public'));
+
+
+app.get('/api/notes', (req, res) => {
+    const notes = getNotes();
+    res.json(notes);
+  });
+  
+  // POST route for creating a new note
+  app.post('/api/notes', (req, res) => {
+    const newNote = req.body;
+    const notes = getNotes();
+    newNote.id = generateUniqueID();
+    const updatedNotes = [...notes, newNote];
+    saveNotes(updatedNotes);
+    res.json(newNote);
+  });
+  
+  // DELETE route for deleting a note
+  app.delete('/api/notes/:id', (req, res) => {
+    const noteId = parseInt(req.params.id);
+    const notes = getNotes();
+    const filteredNotes = notes.filter((note) => note.id !== noteId);
+    saveNotes(filteredNotes);
+    res.sendStatus(204);
+  });
+  
+  // Helper functions
+  
+  // Function to read notes from the database file
+  const getNotes = () => {
+    try {
+      const notesData = fs.readFileSync(path.join(__dirname, 'db', 'db.json'), 'UTF8');
+      const notes = JSON.parse(notesData);
+      console.log('Read notes:', notes);
+      return notes;
+    } catch (err) {
+      console.error('Error reading notes:', err);
+      return [];
+    }
+  };
+  
+  // Function to generate a unique ID for a new note
+  const generateUniqueID = () => {
+    const timestamp = Date.now().toString();
+    const randomNumber = Math.floor(Math.random() * 1000).toString();
+    return timestamp + randomNumber;
+  }
+  
+  // Function to save notes to the database file
+  const saveNotes = (notes) => {
+    fs.writeFileSync(path.join(__dirname, 'db', 'db.json'), JSON.stringify(notes));
+  };
+
 
 
 //GET Route for notes page
